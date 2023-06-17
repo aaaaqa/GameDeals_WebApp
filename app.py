54d7eeb3ae4bb50bb4ad52cf35318cc71f09ab77
Marcelo Guerrero <73970274+mache12V2@users.noreply.github.com>
2023-06-17 00:56:31 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template, request , redirect,url_for
 
 app = Flask(__name__)
+wishlist = []
 
-@app.route("/")
-def home():
-    return "Hello World!"
+def list_append(item):
+    global wishlist
+    wishlist.append(item)
+
+def list_delete(item):
+    global wishlist
+    wishlist.remove(item)
+
+@app.route("/",methods = ['GET','POST'])
+
+def main_wishlist():
+    if request.method == 'POST':
+        item_added = request.form['item']
+        if item_added != '':
+            list_append(item_added)
+        
+    return render_template('index.html',wishlist = wishlist)
+
+@app.route("/delete/<item>")
+
+def delete(item):
+    list_delete(item)
+    return redirect(url_for('main_wishlist'))
+
+app.run(debug=True)
