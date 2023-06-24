00b9d89af4ab3c680a091954399975cf00fb9ba1
Juan Alejandro Benites <thedumbar10@gmail.com>
2023-06-24 16:52:39 -0500

    Review Page
    Se pudo terminar el review page de la pagina web.

--- scripts/review.py
+++ scripts/review.py
@@ -11,17 +11,6 @@ login_manager.init_app(review)
 
 @review.route('/review', methods=['GET', 'POST'])
 @login_required
+
 def show():
-    if request.method == 'POST':
-        file = request.files['inputFile']
-        data = file.read()
-        render_pic = base64.b64encode(data).decode('ascii') 
-        render_file = render_pic
-        newFile = IndieGame(
-                        imageIndie=data, 
-                        rendered_data=render_file)
-        db.session.add(newFile)
-        db.session.commit() 
-        #flash(f'Pic {newFile.nameGame} uploaded Text: {newFile.descriptionGame}')
-        return redirect(url_for('home.show') + '?success=image-uploaded')
     return render_template('review.html')
