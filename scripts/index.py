0d1d2953e11a74de3ff85a7a817c25a4131f4ba0
aaaaqa <u202014558@upc.edu.pe>
2023-06-25 17:28:45 -0500

    Updated small route problem

--- scripts/index.py
+++ scripts/index.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 index = Blueprint('index', __name__, template_folder='../templates')
 
-@index.route('/index', methods=['GET'])
+@index.route('/wishlist', methods=['GET'])
 def show():
     return render_template('index.html')
